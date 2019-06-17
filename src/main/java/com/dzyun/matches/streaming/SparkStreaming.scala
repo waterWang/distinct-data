dc61b9244a986e746b23beb1324d39f253c175a9
wangweiwei <wangww@dazhenyun.cn>
2019-06-17 14:05:17 +0800

    test checkoutpoint

--- src/main/java/com/dzyun/matches/streaming/SparkStreaming.scala
+++ src/main/java/com/dzyun/matches/streaming/SparkStreaming.scala
@@ -24,14 +24,18 @@ object SparkStreaming {
   private val file_name_regex = "\\."
   //  private val hdfs_path = YamlUtil.getPatam("hdfsPath")
   private val file_dir = "hdfs://nameservice2/home/tiger/origin_data_files_test/"
-//  private val file_dir = "file:///home/tiger/distinct-data/data/"
+  private val checkpoint_dir = "hdfs://nameservice2/tiger/streaming_checkpoint_data"
+  //  private val file_dir = "file:///home/tiger/distinct-data/data/"
 
-  //TODO  checkpoint
-  //https://spark.apache.org/docs/2.2.0/streaming-programming-guide.html#checkpointing
-  def funcToCreateContext(): StreamingContext = {
+  /**
+    * https://spark.apache.org/docs/2.2.0/streaming-programming-guide.html#checkpointing
+    * Create spark streamingContext function for getOrCreate method
+    */
+  def createContext(): StreamingContext = {
+
     val conf = new SparkConf().setAppName("distinct-data").setMaster("yarn")
     val ssc = new StreamingContext(conf, Seconds(3))
-    ssc.checkpoint("") // set checkpoint directory
+    ssc.checkpoint(checkpoint_dir)
     ssc
   }
 
@@ -59,8 +63,8 @@ object SparkStreaming {
   }
 
   def main(args: Array[String]) = {
-    val conf = new SparkConf().setAppName("distinct-data").setMaster("yarn")
-    val ssc = new StreamingContext(conf, Seconds(3))
+
+    val ssc = StreamingContext.getOrCreate(checkpoint_dir, createContext _)
     val dStream = namedTextFileStream(ssc, file_dir)
 
     def byFileTransformer(filename: String)(rdd: RDD[String]): RDD[(String, String)] =
@@ -70,41 +74,43 @@ object SparkStreaming {
     data.print()
     log.info("=============start streaming==============")
     val start = System.currentTimeMillis()
-    data.foreachRDD(rdd => {
-      val hives: java.util.List[MsgEntity] = null
-      val hbases: java.util.List[RowEntity] = null
-      rdd.foreach(s => {
-        val filename = s._1.split(file_name_regex)(0)
-        val line = s._2
-        val arr = line.split(line_regex)
-        val rowKey = ShaUtils.encrypt(arr(0), arr(1), arr(3), arr(4))
-        if (!HBaseClient.existsRowKey(rowKey)) {
-          log.info("insert line=" + line)
-          val hiveBean = new MsgEntity()
+    if(null != data){
+      data.foreachRDD(rdd => {
+        val hives: java.util.List[MsgEntity] = null
+        val hbases: java.util.List[RowEntity] = null
+        rdd.foreach(s => {
+          val filename = s._1.split(file_name_regex)(0)
+          val line = s._2
+          val arr = line.split(line_regex)
+          val rowKey = ShaUtils.encrypt(arr(0), arr(1), arr(3), arr(4))
+          if (!HBaseClient.existsRowKey(rowKey)) {
+            log.info("insert line=" + line)
+            val hiveBean = new MsgEntity()
 
-          hiveBean.setPhone_id(arr(0))
-          hiveBean.setCreate_time(arr(1).toLong)
-          hiveBean.setApp_name(arr(2))
-          hiveBean.setMain_call_no(arr(3))
-          hiveBean.setMsg(arr(4))
-          hiveBean.setThe_date(DateUtils.format(arr(1)))
-          hiveBean.setFile_no(filename)
+            hiveBean.setPhone_id(arr(0))
+            hiveBean.setCreate_time(arr(1).toLong)
+            hiveBean.setApp_name(arr(2))
+            hiveBean.setMain_call_no(arr(3))
+            hiveBean.setMsg(arr(4))
+            hiveBean.setThe_date(DateUtils.format(arr(1)))
+            hiveBean.setFile_no(filename)
 
-          val hbaseBean = new RowEntity()
-          hbaseBean.setRowKey(rowKey)
-          hbaseBean.setCol(colName)
-          hbaseBean.setValue(filename)
-          hives.add(hiveBean)
-          hbases.add(hbaseBean)
-        } else {
-          log.error("not insert filename=" + filename + " line=" + line)
+            val hbaseBean = new RowEntity()
+            hbaseBean.setRowKey(rowKey)
+            hbaseBean.setCol(colName)
+            hbaseBean.setValue(filename)
+            hives.add(hiveBean)
+            hbases.add(hbaseBean)
+          } else {
+            log.error("not insert filename=" + filename + " line=" + line)
+          }
+        })
+        if (null != hbases && !hbases.isEmpty) {
+          HBaseClient.batchAdd(hbases)
+          HiveClient.batchAdd(hives)
         }
       })
-      if (!hbases.isEmpty) {
-        HBaseClient.batchAdd(hbases)
-        HiveClient.batchAdd(hives)
-      }
-    })
+    }
     val cost = System.currentTimeMillis() - start
     log.info("=============end streaming,cost time is==============" + cost / 1000)
     ssc.start()
